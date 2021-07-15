7c204e33666c6f3ee57362196821c8e5445cf6f4
Patrick Arminio <patrick.arminio@gmail.com>
2021-07-15 23:39:07 +0100

    Fetch information from a JSON file

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom";
 import { Card } from "./components/card";
 import "./index.css";
 
+import data from "./data.json";
+
 ReactDOM.render(
   <React.StrictMode>
-    <Card version="0.68" contributor="Jake" description="Description" />
+    <Card
+      version={data.version}
+      contributor={data.contributor}
+      description={data.description}
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
